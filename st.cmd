a514557adf22f83d79c8a427a07166de0a7bc17e
dvc-x <dvc@slac.stanford.edu>
2026-08-14 13:41:00 -0700

    Update st.cmd

--- st.cmd
+++ st.cmd
@@ -13,36 +13,16 @@ export EPICS_CA_MAX_ARRAY_BYTES=8000000
 source ${PSPKG_ROOT}/etc/set_env.sh
 
 echo "* Starting up, base=${base}"
-cd exp-timing
 
 echo "$hutch"
 
 case $base in
-   py-fstiming)
-      if [[ $hutch = XCS ]]; then
-         script=femto_longdelay.py
-      else
-         script=femto.py
-      fi
-      export MPLCONFIGDIR=/reg/d/iocData/fstiming
-      ;;
    py-fstiming-tt)
       source /reg/g/pcds/setup/epicsenv-3.14.12.sh
       source /cds/group/pcds/pyps/conda/pcds_conda
       python -V
       script=Drift_Correction.py
       export MPLCONFIGDIR=/reg/d/iocData/fstiming-tt
-      ;;
-   py-fstiming-pcav)
-      script=pcav2ttdrift.py
-      export MPLCONFIGDIR=/reg/d/iocData/fstiming-pcav
-      ;;
-   py-fstiming-cast)
-      source /reg/g/pcds/setup/epicsenv-3.14.12.sh
-      source /cds/group/pcds/pyps/conda/pcds_conda
-      script=pcav2cast_${hutch}.py
-      export MPLCONFIGDIR=/reg/d/iocData/fstiming-cast-${hutch}
-      unset hutch
       ;;
    *)
       echo "Bad IOC name: $IOC"
@@ -52,11 +32,3 @@ case $base in
 esac
 
 echo "Running script $script hutch $hutch"
-
-if [[ $script = pcav2ttdrift.py ]]; then 
-   python $script $hutch -T -P || echo "Script exited with code $?"
-else
-   python $script $hutch || echo "Script exited with code $?" 
-fi
-
-
